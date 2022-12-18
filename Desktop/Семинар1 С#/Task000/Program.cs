59ff544c2268e6c2a5fceafd08f098b5d89d3337
Aigul Aiupova <aigul.sadreeva@gmail.com>
2022-12-18 23:38:11 +0300

    Added Task 000 квадрат числа

---
+++ Desktop/Семинар1 С#/Task000/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая 
+// 1. на вход принимает число 
+// 2. и выдает его квадрат(число,
+// умноженное на само себя).
+// Например:
+// 4 - > 16
+// -3 - > 9
+// -7 -› 49
+
+//string str = Console.ReadLine(); /// 24234. fhffhht
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+//int number1 = int. Parse(Console. ReadLine ( ));
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
