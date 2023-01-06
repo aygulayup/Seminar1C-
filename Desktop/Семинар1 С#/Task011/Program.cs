f6439e31b84da425ef020f9d4e766089c882f537
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-06 23:50:12 +0300

    Решение задач 011, 013

---
+++ Desktop/Семинар1 С#/Task011/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+// удаляет вторую цифру этого числа.
+// выводит на экран двузначное оставшееся число
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+// Решение 1.
+
+// int number = new Random().Next(100,1000);
+// Console.WriteLine(number);
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int result = firstDigit * 10 + thirdDigit;
+// Console.WriteLine(result);
+
+// Решение через метод.
+
+int rnd = new Random( ).Next (100, 1000);
+
+int MakeDigit(int number)
+{
+int result = (number/100)*10 + number % 10;
+return result;
+}
+
+int newDigit = MakeDigit(rnd);
+Console.WriteLine($" {rnd} -> {newDigit}");
---
+++ Desktop/Семинар1 С#/Task013/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет принимать на вход два числа
+// и выводить, является ли первое число кратным второму.
+// Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int div = number1 % number2;
+if (div == 0)
+{
+    Console.WriteLine($"{number1}, {number2} -> кратно");
+}
+else
+{
+    Console.WriteLine($"{number1}, {number2} -> не кратно, остаток {div}");
+}
