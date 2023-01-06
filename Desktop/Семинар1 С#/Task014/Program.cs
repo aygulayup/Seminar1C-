58b58a95ddefff1d0b5647af4f66e27cc13601bd
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-07 00:43:41 +0300

    Решение задач 014 и 016

---
+++ Desktop/Семинар1 С#/Task014/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и 
+// проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 8 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number % 7 == 0 && number % 23 == 0)
+// {
+//     Console.WriteLine($"{number} -> да");
+// }
+// else
+// {
+//     Console.WriteLine($"{number} -> нет");
+// }
+
+Console.WriteLine(DevTwoNum(number) ? "да" : "нет");
+bool DevTwoNum(int num)
+{
+    num % 7 == 0 && num % 23 == 0;
+
+}
+
---
+++ Desktop/Семинар1 С#/Task016/Program.cs
@@ -0,0 +1,30 @@
+﻿// 16. Напишите программу, которая принимает на вход два числа 
+// и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.WriteLine("Введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+// int square1 = number1 * number1;
+// int square2 = number2 * number2;
+// if (square1 == number2 || square2 == number1)
+// {
+//       Console.WriteLine($"{number1}, {number2} -> да");  
+// }
+// else
+// {
+//     Console.WriteLine($"{number1}, {number2} -> нет"); 
+// }
+
+
+
+Console.WriteLine(IsSquare(number1, number2) ? "да" : "нет");
+bool IsSquare(int n1, int n2)
+{
+    return n1 / n2 == n2 || n2 /n1 == n1;
+}
