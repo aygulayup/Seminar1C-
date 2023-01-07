7bfb5d141e9090ab6eb5df9f99836907695d051e
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-08 00:10:31 +0300

    Решение задач 017, 018

---
+++ Desktop/Семинар1 С#/Task017/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход координаты точки (X и Y), 
+// 2. причём X # 0 и Y # 0 и 
+// 3. выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите координаты точки");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(x, y);
+string result = quarter > 0 
+        ? $"указанные координаты соответствуют четверти -> {quarter}" 
+        : "Введены некорректные координаты";
+
+Console.WriteLine(result);
+
+int Quarter(int xc, int yc)
+{
+    if(xc > 0 && yc > 0) return 1;
+    if(xc < 0 && yc > 0) return 2;
+    if(xc < 0 && yc < 0) return 3;
+    if(xc > 0 && yc < 0) return 4;
+    return 0;
+}
---
+++ Desktop/Семинар1 С#/Task018/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая 
+// 1. по заданному номеру четверти, 
+// 2. показывает диапазон возможных координат точек 
+// в этой четверти (х И у).
+
+Console.WriteLine("Введите номер четверти");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string range = Range(num);
+
+Console.WriteLine(range);
+
+string Range(int number)
+{
+    if (number == 1) return "Диапазон координат (x > 0, y > 0)";
+    if (number == 2) return "Диапазон координат (x < 0, y > 0)";
+    if (number == 3) return "Диапазон координат (x < 0, y < 0)";
+    if (number == 4) return "Диапазон координат (x > 0, y < 0)";
+    return "неверная четверть";
+}
