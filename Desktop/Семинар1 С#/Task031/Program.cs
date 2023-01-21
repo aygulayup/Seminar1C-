b472f5f044a2affe36b9a80ab8847aeefcae7bc1
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-21 23:38:13 +0300

    Задача 31 массив.Сумма элементов

---
+++ Desktop/Семинар1 С#/Task031/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 31: 
+// 1. Задайте массив из 12 элементов, 
+// 2. заполненный случайными числами из промежутка [-9, 9]. 
+// 3. Найдите сумму отрицательных и положительных элементов массива.
+
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
+// сумма положительных чисел равна 29, 
+// сумма отрицательных равна - 20.
+
+int[] CreateArrayRndInt(int size, int min, int max) //создание и заполнение массива
+{
+    int [] array = new int[size]; // новое место памяти для массива
+    Random rnd = new Random(); // новое место памяти для элемента класса Random
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int [] array)// вывод массива в консоль
+{
+    Console.Write("["); //вывод массива на экран в [скобках]
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write(array[i] + ", "); // вывод эл-ов массива через запятую
+        else Console.Write(array[i]); // обратиться к последнему элементу массива array[^1]
+    }
+    Console.WriteLine("]");
+}
+
+int GetSumPositiveEl(int[] array) // метод подсчета положит.значений
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) sum += array[i];
+    }
+    return sum;
+}
+
+int GetSumNegativeEl(int[] array) // метод подсчета отриц.значений
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < 0) sum += array[i];
+    }
+    return sum;
+}
+
+
+int[] arr = CreateArrayRndInt(12, -9, 9); //создание массива
+PrintArray(arr); // вывод на экран
+
+int sumPositive = GetSumPositiveEl(arr);
+int sumNegative = GetSumNegativeEl(arr);
+
+Console.WriteLine($"Сумма положительных элементов = {sumPositive}");
+Console.WriteLine($"Сумма негативных элементов = {sumNegative}");
