03b2194a50cfeee9951c4dab51b439f4f13e4894
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-02-04 22:22:23 +0300

    Семинар 8

---
+++ Desktop/Семинар1 С#/Task057/Program.cs
@@ -0,0 +1,110 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+int[,] CreateMatrixRndInt(int rows, int colomns, int min, int max) //создание и заполнение массива случайными элементами
+{
+    int [,] matrix = new int[rows, colomns]; // новое место памяти для массива
+    Random rnd = new Random(); // новое место памяти для элемента класса Random
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int [,] matrix)// вывод массива в консоль
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+ 
+    Console.Write("["); //вывод массива на экран в [скобках]
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if(j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, "); // вывод эл-ов массива через запятую
+        else Console.Write($"{matrix[i, j], 4} "); // обратиться к последнему элементу массива array[^1]
+    }
+    Console.WriteLine("]");
+    }
+}
+
+int[] MatrixToArray(int[,] matrix)
+{
+    int [] array = new int[matrix.Length]; //int size = matrix.GetLength(0) * matrix.GetLength(1);
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[k] = matrix[i, j];
+            k++;
+        } 
+    }
+    return array;
+}
+
+void PrintArray(int [] array)// вывод массива в консоль
+{
+    Console.Write("["); //вывод массива на экран в [скобках]
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write(array[i] + ", "); // вывод эл-ов массива через запятую
+        else Console.Write(array[i]); // обратиться к последнему элементу массива array[^1]
+    }
+    Console.WriteLine("]");
+}
+
+void CountDictionary(int [] array)
+{
+    int count = 1;
+    int num = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] == num) count++;
+        else
+        {
+            Console.WriteLine($"Количество чисел {num} = {count}");
+            count = 1; // обнуляем счетчик, чтобы заново начать отсчет чисел
+            num = array[i];
+        }
+    }
+    Console.WriteLine($"Количество чисел {num} = {count}");
+}
+
+// int[,] CreateDictionaryFromArray(int[] array)
+// {
+// int[,] dictionary = new int [array.Length, 2];
+// int countNumbers = array[0];
+// dictionary[0, 0] = countNumbers;
+// dictionary[0, 1] = 0;
+// int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] == countNumbers)
+//         {
+//             dictionary[count, 1] ++;
+//         }
+//         else
+//         {
+//             count ++;
+//             dictionary[count, 0] = array[i];
+//             dictionary[count, 1] ++;
+//             countNumbers = array[i];
+//         }
+//     }
+//     return dictionary;
+// }
+
+
+int[,] arr2D = CreateMatrixRndInt(3, 3, 0, 10);
+PrintMatrix(arr2D);
+Console.WriteLine();
+int[] arr = MatrixToArray(arr2D);
+Array.Sort(arr);
+PrintArray(arr);
+CountDictionary(arr);
+// int[,] dictionaryFromArray = CreateDictionaryFromArray(arr);
+// PrintMatrix(dictionaryFromArray);
+
---
+++ Desktop/Семинар1 С#/Task059/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел.
+// Напишите программу, которая 
+// удалит строку и столбец, на пересечении которых 
+// расположен наименьший элемент массива.
+// Например, задан массив: Наименьший элемент - 1, на выходе получим следующий массив:
+// 1 4 7 2                     9 2 3
+// 5 9 2 3                     4 2 4
+// 8 4 2 4                     2 6 7
+// 5 2 6 7
+
+
+
+
+
+int[,] CreateMatrixRndInt(int rows, int colomns, int min, int max) //создание и заполнение массива случайными элементами
+{
+    int [,] matrix = new int[rows, colomns]; // новое место памяти для массива
+    Random rnd = new Random(); // новое место памяти для элемента класса Random
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int [,] matrix)// вывод массива в консоль
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+ 
+    Console.Write("["); //вывод массива на экран в [скобках]
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if(j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, "); // вывод эл-ов массива через запятую
+        else Console.Write($"{matrix[i, j], 4} "); // обратиться к последнему элементу массива array[^1]
+    }
+    Console.WriteLine("]");
+    }
+}
+
+(int row, int column, int minValue) IndexMinValue(int[,] matrix) //
+{
+    int minValueRow = 0;
+    int minValueColumn = 0;
+    int minValue = matrix[0,0];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(minValue > matrix[i, j])
+            {
+                minValue = matrix[i, j];
+                minValueRow = i;
+                minValueColumn = j;
+
+            }
+        }
+    }
+    return (minValueRow, minValueColumn, minValue);
+}
+
+int[,] arr2D = CreateMatrixRndInt(3, 3, 0, 10);
+PrintMatrix(arr2D);
+Console.WriteLine();
+var indexMinValue = IndexMinValue(arr2D);
+Console.WriteLine(indexMinValue.row + "\n" + indexMinValue.column + "\n" + indexMinValue.minValue);// "\n" перевод на след.строку
